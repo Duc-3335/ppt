f71baf1f194e33616f3e4a1920dfff6c230972dc
Duc <Duc.PM233335@sis.hust.edu.vn>
2025-02-28 22:05:58 +0700

    new

---
+++ xapxi.py
@@ -0,0 +1,11 @@
+from scipy.misc import derivative
+import numpy as np
+def func(x):
+    return x**10 - 2
+
+def bowstring(a, b, func):
+    x = np.linspace(a, b, int(1e5))
+    
+        
+
+bowstring(-2, -0.1)        
